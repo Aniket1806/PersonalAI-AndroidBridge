48067c79ca98a2c7b41311b057a2e507dacf58ac
Anny <aniketsangashetti110@gmail.com>
2026-07-20 08:35:12 +0530

    Initial Android Bridge

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.personalai.bridge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.personalai.bridge"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+dependencies {
+}
